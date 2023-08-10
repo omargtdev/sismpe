061dc4d9d331ead2faf99f1b025a00cc4d0779e9
omargtdev <omargtdev@gmail.com>
2023-08-10 02:16:07 -0500

    fix(register): JS added to allow interact with form

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,17 @@ const hamburger = document.querySelector(".hamburger");
 const navbarList = document.querySelector(".navbar__list");
 
 hamburger.addEventListener("click", function() {
-	hamburger.classList.toggle("active");
-	navbarList.classList.toggle("active");
+	const formContainer = document.querySelector(".form-container");
+	if(!hamburger.classList.contains("active")) {
+		if(formContainer)
+			formContainer.style.zIndex = -1;
+		hamburger.classList.add("active");
+		navbarList.classList.add("active");
+		return;
+	}
+
+	if(formContainer)
+		formContainer.style.zIndex = 0;
+	hamburger.classList.remove("active");
+	navbarList.classList.remove("active");
 }); 
